84882379a54504bae1d0f11a8d7681b9313cb66c
Rahul M Gala <z.rahulgala@gmail.com>
2018-07-20 01:08:58 -0700

    Update linear_search.py

--- linear_search/linear_search.py
+++ linear_search/linear_search.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python2.7 -tt
 
+#Notes:
+#to run type: python linear_search.py in terminal
+#you can change the number in array thevalues
+
+import sys
+            
+def main():
+    thevalues = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10]
+    target = 6
+    linearsearch(thevalues, target)
+
+def linearsearch(thevalues, target):
+    n = len(thevalues)
+    for i in range (n):
+        if thevalues[i] == target:
+            print("match found")  
+
+if __name__== '__main__':
+    main()
